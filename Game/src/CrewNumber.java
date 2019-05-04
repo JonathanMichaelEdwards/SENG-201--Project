c6c49fe924c559d0f2cec34e848819bb3360e8b5
Jonathan <jon.michael.edwards13@gmail.com>
2019-05-04 15:06:20 +1200

    Design

--- Game/src/CrewNumber.java
+++ Game/src/CrewNumber.java
@@ -1,5 +1,12 @@
 import java.awt.EventQueue;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JSlider;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class CrewNumber 
@@ -15,8 +22,35 @@ public class CrewNumber
 	{
 		Settings set = new Settings();
 		frame = new JFrame();
-		frame.setBounds(set.x, set.y, set.width, set.height);
+		frame.setBounds(set.x, set.y, 828, 522);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("How many Crew members do you want?");
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 25));
+		lblNewLabel.setBounds(141, 44, 595, 109);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JSlider slider = new JSlider();
+		slider.setBounds(247, 209, 356, 68);
+		frame.getContentPane().add(slider);
+		
+		JLabel lblCrewNumber = new JLabel("Crew number ...");
+		lblCrewNumber.setFont(new Font("Dialog", Font.BOLD, 16));
+		lblCrewNumber.setBounds(351, 289, 166, 16);
+		frame.getContentPane().add(lblCrewNumber);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(454, 380, 136, 75);
+		frame.getContentPane().add(btnBack);
+		
+		JButton btnNewButton = new JButton("Next");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(663, 380, 136, 75);
+		frame.getContentPane().add(btnNewButton);
 	}
 	
 	
---
+++ Game/src/NameShip.java
@@ -0,0 +1,104 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JList;
+import javax.swing.AbstractListModel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+
+public class NameShip {
+
+	private JFrame frame;
+	private JTextField textField;
+
+	
+	/*
+	 * Initialize the contents of the frame.
+	*/
+	private void initialize() 
+	{
+		frame = new JFrame();
+		frame.setBounds(100, 100, 851, 523);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Crew:");
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 19));
+		lblNewLabel.setBounds(40, 126, 81, 53);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNameYourShip = new JLabel("Name Your ship");
+		lblNameYourShip.setFont(new Font("Dialog", Font.BOLD, 25));
+		lblNameYourShip.setBounds(298, 12, 253, 107);
+		frame.getContentPane().add(lblNameYourShip);
+		
+		JList list = new JList();
+		list.setToolTipText("1");
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] {"member 1", "member 2", "..."};
+			public int getSize() {
+				return values.length;
+			}
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		list.setBounds(39, 191, 98, 194);
+		frame.getContentPane().add(list);
+		
+		textField = new JTextField();
+		textField.setBounds(560, 244, 186, 68);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblShipType = new JLabel("Ship Type");
+		lblShipType.setFont(new Font("Dialog", Font.BOLD, 19));
+		lblShipType.setBounds(298, 126, 125, 94);
+		frame.getContentPane().add(lblShipType);
+		
+		JLabel lblNameShip = new JLabel("Name Ship");
+		lblNameShip.setFont(new Font("Dialog", Font.BOLD, 19));
+		lblNameShip.setBounds(560, 126, 134, 94);
+		frame.getContentPane().add(lblNameShip);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(504, 374, 125, 77);
+		frame.getContentPane().add(btnBack);
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.setBounds(672, 374, 125, 77);
+		frame.getContentPane().add(btnStart);
+	}
+	
+	
+	/*
+	 * Create the application.
+	*/
+	public NameShip() 
+	{
+		initialize();
+	}
+	
+	
+	/*
+	 * Launch the application.
+	*/
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try {
+					NameShip window = new NameShip();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
