1077cbcd75bb397d2bc39762b1b4a54b89c48716
Jonathan <jon.michael.edwards13@gmail.com>
2019-05-04 16:49:41 +1200

    Choose Days

--- Game/src/ChooseDays.java
+++ Game/src/ChooseDays.java
@@ -4,7 +4,7 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JSlider;
 import javax.swing.JButton;
-
+import java.util.Hashtable;
 
 
 public class ChooseDays 
@@ -35,6 +35,23 @@ public class ChooseDays
 		slider.setMinimum(3);
 		slider.setMaximum(10);
 		slider.setBounds(276, 282, 406, 135);
+		slider.setMajorTickSpacing(1);
+		slider.setPaintTicks(true);
+		// Set the labels to be painted on the slider
+		slider.setPaintLabels(true);
+		         
+		// Add positions label in the slider
+		Hashtable<Integer, JLabel> position = new Hashtable<Integer, JLabel>();
+		position.put(3, new JLabel("3"));
+		position.put(4, new JLabel("4"));
+		position.put(5, new JLabel("5"));
+		position.put(6, new JLabel("6"));
+		position.put(7, new JLabel("7"));
+		position.put(8, new JLabel("8"));
+		position.put(9, new JLabel("9"));
+		position.put(10, new JLabel("10"));
+		// Set the label to be drawn
+		slider.setLabelTable(position); 
 		frame.getContentPane().add(slider);
 		
 		JLabel lblHowManyDays = new JLabel("How many Days?");
